ccf4678a30fbec390aec68c75d7d5783b22f0c1e
Nutthawut123 <68680249+Nutthawut123@users.noreply.github.com>
2020-11-07 20:18:22 +0700

    start

---
+++ layouts/default.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-app dark>
+    <v-main>
+      <v-container>
+        <nuxt />
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<style>
+.theme--dark.v-application {
+  background-image: url('https://obs.line-scdn.net/0hVh5M2jVZCUcINCA2jt92EDJiCig7WBpEbAJYRFRaV3NyBEkYZwEUciQwVycmU04ZZgZOKC83EnZ3AE1CNwIU/w1200');
+  background-size: cover;
+}
+h1 {
+  font-family: 'Amatic SC', cursive;
+  color: #000000;
+}
+h2 {
+  font-family: 'Amatic SC', cursive;
+  color: #212121;
+}
+.mor {
+  font-family: 'Sansita Swashed', cursive;
+  color: #ffffff;
+  font-size: 6rem;
+  text-align: center;
+}
+</style>
